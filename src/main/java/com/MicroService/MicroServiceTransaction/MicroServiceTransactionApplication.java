65348d0cc6f35a4fb2d3c82b605bdc0b08200ac1
Sebastian Sepulveda Patiño <sebas20112322@gmail.com>
2024-09-09 16:28:36 -0500

    Creación del microservicio

---
+++ src/main/java/com/MicroService/MicroServiceTransaction/MicroServiceTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.MicroService.MicroServiceTransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceTransactionApplication.class, args);
+	}
+
+}
